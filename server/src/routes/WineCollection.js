38a98407c4ebbdffb6e02caf2881639a5514e849
Mahita07 <89206252+Mahita07@users.noreply.github.com>
2023-07-15 22:13:53 +0530

    more routes for wines, updated user schema

--- server/src/routes/WineCollection.js
+++ server/src/routes/WineCollection.js
@@ -24,4 +24,42 @@ router.post("/", async(req,res)=>{
             res.json(err);
         }
 })
+
+//saving a wine to favourites
+router.put("/", async(req,res) =>{
+    try{
+        const wine = await WineModel(req.body.wineId);
+        const user = await UserModel(req.body.userId);
+        user.savedWines.push(wine);
+        await user.save();
+        res.json({savedWines: user.savedWines});
+    }
+    catch(err){
+        res.json(err);
+    }
+})
+
+//get saved wines' ids' 
+router.get("/savedWines/ids", async(req,res) =>{
+    try{
+        const user = await UserModel.findById(req.body.userId);
+        //return the wine id s of the wines saved by user
+        res.json({savedWines:user?.savedWines})
+    }
+    catch(err){
+        res.json(err);
+    }
+})
+
+//get the saved wine documents
+router.get("/savedWines/ids", async(req,res) =>{
+    try{
+        const user = await UserModel.findById(req.body.userId);
+        const savedWines = await WineModel.find({_id: {$in:user?.savedWines},});
+        res.json({savedWines})
+    }
+    catch(err){
+        res.json(err);
+    }
+})
 export {router as wineCollectionRouter};
